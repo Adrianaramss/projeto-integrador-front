b7178c315e05e17a70ac991e336db526685f6137
Adriana <adrianascosta9@gmail.com>
2023-03-02 14:21:33 -0300

    Estilização-login

---
+++ src/Pages/login/style.js
@@ -0,0 +1,96 @@
+import styled from "styled-components"
+
+
+export const Container = styled.div`
+div{
+  margin-top: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+}
+
+ `
+export const Input = styled.input`
+width: 363px;
+height: 60px;
+margin: 0 auto;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+margin-bottom: 20px;
+
+`
+export const Button = styled.button`
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin: 0 auto;
+align-items: center;
+padding: 13px 133px;
+gap: 10px;
+width: 365px;
+height: 51px;
+left: 29px;
+top: 587px;
+border-radius: 27px;
+background: linear-gradient(90deg, #FF6489 0%, #F9B24E 100%);
+border-radius: 27px;
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+line-height: 25px;
+text-align: center;
+color: #FFFFFF;
+border: none;
+margin-top: 10%;
+
+
+`
+
+export const ButtonConta = styled.button`
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin: 0 auto;
+align-items: center;
+padding: 13px 90px;
+gap: 10px;
+width: 365px;
+height: 51px;
+left: 29px;
+top: 587px;
+border: 1px solid #FE7E02;
+border-radius: 27px;
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+line-height: 25px;
+text-align: center;
+color: #FE7E02;
+flex: none;
+order: 0;
+flex-grow: 0;
+background-color: white;
+margin-bottom: 10%;
+
+
+
+`
+
+export const Div = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+height: 1px;
+background: linear-gradient(90deg, #FF6489 0%, #F9B24E 100%);
+border: none;
+margin: 20px;
+
+
+`
