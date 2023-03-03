257713370ff80d8c394f2790b9fbcef6b4454df4
Adriana <adrianascosta9@gmail.com>
2023-03-02 22:02:01 -0300

    estilização-signup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import { Router } from './routes'
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  
+  }
+`;
+
 
 const App = () => {
     return (
+      <>
+      <GlobalStyle/>
       <Router/>
+      </>
     )
 }
 export default App;
---
+++ src/Pages/signup/style.js
@@ -0,0 +1,111 @@
+import styled from "styled-components"
+
+
+
+
+
+export const TextoSignup = styled.h1`
+width: 364px;
+height: 86px;
+left: 32px;
+top: 123px;
+font-family: 'IBM Plex Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 33px;
+line-height: 43px;
+color: #373737;
+margin-top: 20px;
+margin-bottom:50px;
+`
+
+export const Header = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: #EDEDED ;
+width: 100%;
+height: 50px;
+
+  
+img{
+
+width: 28.02px;
+height: 28.64px;
+left: 199.99px;
+top: 54.91px;
+}
+
+h2{
+  position: absolute;
+  width: 55px;
+    height: 77px;
+    top: 13px;
+    font-family: 'Noto Sans';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 25px;
+    color: #4088CB;
+    justify-content: flex-end;
+    margin-left: 135px;
+    margin-bottom: 50x;
+    
+}
+
+
+`
+
+export const Input = styled.input`
+width: 363px;
+height: 60px;
+margin: 0 auto;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+margin-bottom: 20px;
+
+`
+
+export const Button = styled.button`
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin: 0 auto;
+align-items: center;
+padding: 13px 133px;
+gap: 10px;
+width: 365px;
+height: 51px;
+left: 29px;
+top: 587px;
+border-radius: 27px;
+background: linear-gradient(90deg, #FF6489 0%, #F9B24E 100%);
+border-radius: 27px;
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+line-height: 25px;
+text-align: center;
+color: #FFFFFF;
+border: none;
+margin-top: 10%;
+
+
+`
+
+export const Texto = styled.p`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin: 0 auto;
+width: 364px;
+height: 93px;
+left: 34px;
+top: 700px;
+margin-top: 50px;
+
+`
