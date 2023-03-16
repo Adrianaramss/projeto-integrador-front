3b50973eb148b7c661f002a6852ba7e721ddad47
Adriana <adrianascosta9@gmail.com>
2023-03-16 16:35:20 -0300

    like-post

--- src/Components/cardpost/index.js
+++ src/Components/cardpost/index.js
@@ -1,22 +1,47 @@
 import { CardPost} from "./style"
-import like from "../../assets/Like.png"
 import dislike from "../../assets/dislike.png"
+import like1 from "../../assets/Like.png"
 import comentario from "../../assets/comentario.png"
 import { useContext,useState } from "react"
 import { GlobalContext } from "../../context/GlobalContext"
 import axios from "axios"
+
+
 export const Card = (props) => {
     const { postagem } = props;
-
+    const context = useContext(GlobalContext);
+    const { fetchPostagem } = context;
   
-   
-    
+    const [isLoading, setIsLoading] = useState(false)
+  
+    const like = async () => {
+      setIsLoading(true)
+  
+      try {
+        const token = window.localStorage.getItem(`teste`);
+  
+        const config = {
+          headers: {
+            Authorization: token
+          }
+        };
+  
+        const body = {
+          like: true
+        }
+  
+       await axios.put(`${process.env.REACT_APP_BASE_URL}/posts/${postagem.id}/like`,body,config);
 
+        setIsLoading(false)
+        fetchPostagem()
+      } catch (error) {
+        console.error(error?.response?.data);
+        window.alert(error?.response?.data)
+      }
+    };
+  
+  
 
-
-
-
-    
     return(
         <CardPost> 
             <div>
@@ -24,9 +49,9 @@ export const Card = (props) => {
            <p>{props.postagem.content} </p>
            
            <div>
-            <span className="likeedislike">
-                <img src={like}  alt= "like"/>
-                1.2K
+            <span className="likeedislike" >
+                <img  src={like1}  onClick={()=>like(postagem.id)} style={{ cursor: "pointer" }}   alt= "like"/>
+                {" "}   {postagem.likes}
                 <img src={dislike} alt= "dislike"/>
             </span>
               
